58a827a53f71c5100cccfe4d556f43fcbf499247
RogerSabau <99741210+RogerSabau@users.noreply.github.com>
2022-07-22 14:12:58 +0200

    new files

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function oefening1vraag1F() {
+  var oefening1vraag1Ant = 2666;
+  var oefening1vraag1 = prompt("1234 + 1432 =")
+
+  if (oefening1vraag1 == oefening1vraag1Ant) {
+    alert("goed")
+  } else {
+    alert("fout")
+  }
+}
+
+function oefening1vraag2F() {
+  var oefening1vraag2Ant = 2955;
+  var oefening1vraag2 = prompt("1532 + 1423 =")
+
+  if (oefening1vraag2 == oefening1vraag1Ant) {
+    alert("goed")
+  } else {
+    alert("fout")
+  }
+}
+
+function oefening1vraag3F() {
+  var oefening1vraag3Ant = 2857;
+  var oefening1vraag3 = prompt("1434 + 1423 =")
+
+  if (oefening1vraag3 == oefening1vraag3Ant) {
+    alert("goed")
+  } else {
+    alert("fout")
+  }
+}
